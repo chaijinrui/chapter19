668d380852712154be7f86284af1ca94cad61853
sns-chaijinrui <chai0304@163.com>
2023-09-21 18:44:44 +0800

    1111

--- src/main/java/com/hspedu/outputstream_/Dog.java
+++ src/main/java/com/hspedu/outputstream_/Dog.java
@@ -13,11 +13,14 @@ import java.io.Serializable;
 public class Dog implements Serializable {
     private String name;
     private int age;
+    private String description;
+    private static final long serialVersionUID = 1L;
 
     public Dog(String name, int age) {
         this.name = name;
         this.age = age;
     }
+
     public String getName() {
         return name;
     }
